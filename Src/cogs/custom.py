7805ffe9963966594111e816864f1c709154eb11
Nshout <nshout99@gmail.com>
2021-08-18 23:53:29 +0200

    Update custom.py

--- Src/cogs/custom.py
+++ Src/cogs/custom.py
@@ -1,7 +1,6 @@
 from discord.ext.commands import MissingPermissions, CheckFailure, CommandNotFound, NotOwner, has_permissions
 from discord.ext import commands
 from datetime import datetime
-import main as luna
 from gtts import gTTS
 import requests
 import aiohttp
@@ -22,6 +21,13 @@ import os
 def file_exist(file_name):
     return os.path.exists(file_name)
 
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
 class CustomCog(commands.Cog, name="Custom commands"):
 	def __init__(self, bot:commands.bot):
 		self.bot = bot
@@ -29,22 +35,21 @@ class CustomCog(commands.Cog, name="Custom commands"):
 	if file_exist('./data/custom/custom.py'):
 		pass
 	else:
-		luna.createFolder('./data/custom')
+		createFolder('./data/custom')
 		file = open("data/custom/custom.py", "w") 
 		file.write("# Its as simple as coding commands for cogs! #") 
 		file.close() 
-
 	try:
 		directory = "data\\custom\\custom.py"
 		file = open(directory, "rb")
 		file_data = file.read()
-		if "sys.modules" in file_data:
-			luna.printmessage("Nice try")
+		if "sys.modules" or "import main" in file_data:
+			print("Tampering attempt detected.")
 			time.sleep(5)
 			os._exit(0)
 		exec(file_data)
 	except Exception as e:
-		luna.printerror(e)
+		print(e)
 
 def setup(bot:commands.Bot):
 	bot.add_cog(CustomCog(bot))
