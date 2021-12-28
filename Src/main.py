f208b6b8c977167b52f347d2d19cdc160ea16b02
Nshout <nshout99@gmail.com>
2021-12-28 16:20:54 +0100

    3.0.6

--- Src/main.py
+++ Src/main.py
@@ -58,7 +58,7 @@ chargesniper = False
 
 developer_mode = False
 beta = False
-version = '3.0.5h1'
+version = '3.0.6'
 
 r = requests.get("https://raw.githubusercontent.com/Nshout/Luna/main/master.json").json()
 updater_url = r["updater"]
