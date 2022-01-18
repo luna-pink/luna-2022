2e467f5b7127ff94b419496ac03fc987e51a7f23
Nshout <nshout99@gmail.com>
2022-01-18 21:44:23 +0100

    2.0.0a Bypass

--- Src/Nuitka/discord/__init__.py
+++ Src/Nuitka/discord/__init__.py
@@ -58,6 +58,7 @@ from .audit_logs import AuditLogChanges, AuditLogEntry, AuditLogDiff
 from .raw_models import *
 from .team import *
 from .sticker import Sticker
+from .bypass import *
 
 _VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
--- Src/discord/bypass/http.py
+++ Src/discord/bypass/http.py
@@ -1,7 +1,9 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2021-present Dolfies
+Luna Selfbot Discord Bypass
+
+Copyright (c) 2022-present Nshout
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
