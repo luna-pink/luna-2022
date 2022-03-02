4780f680dd177f0c8f23a17fb18974b9211e0b82
Nshout <nshout99@gmail.com>
2022-03-02 20:03:58 +0100

    rebase

--- Scripts/pack.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cls
-pyarmor pack --clean -e "--onefile  --icon C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Images/Luna_Logo.ico --console --noconfirm --hidden-import clint.textui --add-data C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Src/cogs;cogs/" --output "C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Scripts" --name "Luna.exe" C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Src/main.py
-py upload.py
