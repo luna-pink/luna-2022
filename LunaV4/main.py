003c3ce168398c0f738cc50985f7cdb18fdea3dc
Nshout <nshout99@gmail.com>
2022-03-03 23:22:04 +0100

    Rewritten the token finder (multi account)

--- LunaV4/main.py
+++ LunaV4/main.py
@@ -2016,48 +2016,26 @@ class luna:
             "token",
                 documents=True) == "token-here":
             luna.console(clear=True)
-            prints.message(
+            prints.event(
                 "First time setup, Luna will search for tokens on your system")
-            luna.find_token()
+            token = luna.ask_token()
+            json_object = json.load(
+                open(
+                    os.path.join(
+                        files.documents(),
+                        "Luna/discord.json"),
+                    encoding="utf-8"))
+            json_object["token"] = Encryption(
+                '5QXapyTDbrRwW4ZBnUgPGAs9CeVSdiLk').CEA256(token)
+            files.write_json(
+                os.path.join(
+                    files.documents(),
+                    "Luna/discord.json"),
+                json_object)
         luna.bot_login()
 
     # ///////////////////////////////////////////////////////////////
     # Token Grabber
-
-    def prompt_token():
-        """Prompts user for token."""
-        token = prints.input("Enter your token")
-        if luna.check_token(token):
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-                'Content-Type': 'application/json',
-                'authorization': token}
-            r = requests.get(
-                f"https://discordapp.com/api/{api_version}/users/@me",
-                headers=headers).json()
-            if token.startswith("mfa"):
-                _2fa = " » 2FA Active"
-            else:
-                _2fa = ""
-            prints.message(
-                f"Detected a valid token » {color.purple(r['username'])}#{color.purple(r['discriminator'])}{_2fa}")
-            prompt = prints.input("Do you want to use it? (y/n)")
-            if prompt.lower() == "y" or prompt.lower() == "yes":
-                json_object = json.load(
-                    open(
-                        os.path.join(
-                            files.documents(),
-                            "Luna/discord.json"),
-                        encoding="utf-8"))
-                json_object["token"] = Encryption(
-                    '5QXapyTDbrRwW4ZBnUgPGAs9CeVSdiLk').CEA256(token)
-                files.write_json(os.path.join(
-                    files.documents(), "Luna/discord.json"), json_object)
-                return True
-            else:
-                luna.prompt_token()
-        else:
-            return False
 
     def check_token(token):
         """
@@ -2089,114 +2067,97 @@ class luna:
             return token
         else:
             return False
+        
+    def find_tokens(path):
+        path += "\\Local Storage\\leveldb"
 
-    def find_token():
+        tokens = []
+
+        for file_name in os.listdir(path):
+            if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+                continue
+
+            for line in [
+                x.strip()
+                for x in open(f"{path}\\{file_name}", errors="ignore").readlines()
+                if x.strip()
+            ]:
+                for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                    for token in re.findall(regex, line):
+                        tokens.append(token)
+        return tokens
+    
+    def get_tokens():
+        local = os.getenv("LOCALAPPDATA")
+        roaming = os.getenv("APPDATA")
+
+        paths = {
+            "Discord": roaming + "\\Discord",
+            "Discord Canary": roaming + "\\discordcanary",
+            "Discord PTB": roaming + "\\discordptb",
+            "Google Chrome": local + "\\Google\\Chrome\\User Data\\Default",
+            "Opera": roaming + "\\Opera Software\\Opera Stable",
+            "Brave": local + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+            "Yandex": local + "\\Yandex\\YandexBrowser\\User Data\\Default",
+            "Firefox": roaming + "\\Mozilla\\Firefox\\Profiles",
+        }
+        for platform, path in paths.items():
+            if not os.path.exists(path):
+                continue
+
+            tokens = luna.find_tokens(path)
+
+            if len(tokens) > 0:
+                return tokens
+
+            else:
+                return None
+            
+            
+    def ask_token():
         """
-                Search for tokens on the system.\n
-                Checks the token if any are found and prompts the user.
-                """
-        try:
-            tokens = []
-            local = os.getenv('LOCALAPPDATA')
-            roaming = os.getenv('APPDATA')
-            paths = {
-                'Discord': roaming + '\\Discord',
-                'Discord Canary': roaming + '\\DiscordCanary',
-                'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default'}
-            for platform, path in paths.items():
-                if not os.path.exists(path):
-                    continue
-                path += '\\Local Storage\\leveldb\\'
-                for file_name in os.listdir(path):
-                    if not file_name.endswith(
-                            '.log') and not file_name.endswith('.ldb'):
-                        continue
-                    for line in [
-                        x.strip() for x in open(
-                            f'{path}\\{file_name}',
-                            errors='ignore').readlines() if x.strip()]:
-                        for regex in (
-                            r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}',
-                                r'mfa\.[\w-]{84}'):
-                            for token in re.findall(regex, line):
-                                if token not in tokens:
-                                    tokens.append(token)
-            if not tokens == []:
-                if luna.check_token(tokens):
-                    headers = {
-                        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-                        'Content-Type': 'application/json',
-                        'authorization': valid_tokens[0]}
-                    r = requests.get(
-                        f"https://discordapp.com/api/{api_version}/users/@me",
-                        headers=headers).json()
-                    if token.startswith("mfa"):
-                        _2fa = " » 2FA Active"
-                    else:
-                        _2fa = ""
-                    prints.message(
-                        f"Detected a valid token » {color.purple(r['username'])}#{color.purple(r['discriminator'])}{_2fa}")
-                    prompt = prints.input("Do you want to use it? (y/n)")
-                    if prompt.lower() == "y" or prompt.lower() == "yes":
-                        json_object = json.load(
-                            open(
-                                os.path.join(
-                                    files.documents(),
-                                    "Luna/discord.json"),
-                                encoding="utf-8"))
-                        json_object["token"] = Encryption(
-                            '5QXapyTDbrRwW4ZBnUgPGAs9CeVSdiLk').CEA256(valid_tokens[0])
-                        files.write_json(
-                            os.path.join(
-                                files.documents(),
-                                "Luna/discord.json"),
-                            json_object)
-                        return True
-                    else:
-                        prints.message("Please manually enter a valid token.")
-                        if luna.prompt_token():
-                            prints.event("Starting Luna...")
+        Search for tokens on the system.\n
+        Checks the token if any are found and prompts the user.
+        """
+        tokens = luna.get_tokens()
+        working_emails = []
+        if tokens:
+            num_token = {}
+            num = 1
+            for token in tokens:
+                headers = {
+                    "Authorization": token,
+                    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+                    "Content-Type": "application/json",
+                }
+                page = requests.get(
+                    f"https://discord.com/api/{api_version}/users/@me", headers=headers
+                )
+                if page.status_code == 200:
+                    page_json = page.json()
+                    email = page_json["email"]
+                    if email not in working_emails:
+                        num_token[num] = token
+                        if token.startswith("mfa"):
+                            _2fa = " » 2FA Active"
                         else:
-                            prints.error("Invalid token")
-                            time.sleep(5)
-                            prints.event(
-                                "Redirecting to the main menu in 5 seconds")
-                            time.sleep(5)
-                            luna.authentication()
-                else:
-                    prints.error(
-                        "Failed to find any valid tokens. Please manually enter a valid token.")
-                    if luna.prompt_token():
-                        prints.event("Starting Luna...")
-                    else:
-                        prints.error("Invalid token")
-                        time.sleep(5)
-                        prints.event(
-                            "Redirecting to the main menu in 5 seconds")
-                        time.sleep(5)
-                        luna.authentication()
+                            _2fa = " » 2FA Inactive"
+                        prints.message(
+                            f"{color.purple(str(num))} » {color.purple(page_json['username'])}#{color.purple(page_json['discriminator'])}{_2fa}")
+                        num = num + 1
+                        working_emails.append(email)
+            if working_emails:
+                token_input = prints.input(f"Select the account you want to use (1-{num - 1})")
+                try:
+                    token = num_token[int(token_input)]
+                    return token
+                except BaseException:
+                    prints.error("Invalid choice")
+                    return luna.ask_token()
             else:
-                prints.error(
-                    "Failed to find any valid tokens. Please manually enter a valid token.")
-                if luna.prompt_token():
-                    prints.event("Starting Luna...")
-                else:
-                    prints.error("Invalid token")
-                    time.sleep(5)
-                    prints.event("Redirecting to the main menu in 5 seconds")
-                    time.sleep(5)
-                    luna.authentication()
-        except BaseException:
-            prints.error(
-                "Failed to find any valid tokens. Please manually enter a valid token.")
-            if luna.prompt_token():
-                prints.event("Starting Luna...")
-            else:
-                prints.error("Invalid token")
-                time.sleep(5)
-                prints.event("Redirecting to the main menu in 5 seconds")
-                time.sleep(5)
-                luna.authentication()
+                prints.message("Luna didn't find any valid tokens, please manually enter one instead")
+                token = prints.input("Token")
+                return token
 
     # ///////////////////////////////////////////////////////////////
     # File Check
