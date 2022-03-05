f0b228cabf07685332842d72dfee0928570b4d85
Nshout <nshout99@gmail.com>
2022-03-05 16:13:33 +0100

    Changed packs

--- Scripts/pack.bat
+++ Scripts/pack.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
-pyarmor pack --clean -e "--onefile  --icon C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Images/Luna_Logo.ico --console --noconfirm --hidden-import clint.textui --add-data C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Luna/cogs;cogs/" --output "C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Scripts" --name "Luna.exe" C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Luna/main.py
+pyarmor pack --clean -e "--onefile  --icon C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Images/Luna_Logo.ico --console --noconfirm --hidden-import clint.textui --add-data C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Luna/cogs;cogs/" --output "C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Scripts" --name "Luna.exe" -x "advanced 2" C:/Users/%USERNAME%/Documents/GitHub/Luna-SelfBot/Luna/main.py
 py upload.py
