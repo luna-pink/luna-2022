82b2d8031cc9a6d362dee9c829c3a8f01a7e943c
Nshout <nshout99@gmail.com>
2022-03-07 23:02:33 +0100

    wrapper changes

--- Luna/wrapper.py
+++ Luna/wrapper.py
@@ -7,8 +7,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext import commands
 from pytz import utc
 
-# token = 'mfa.RKXALnKQEhlV9SOfFKbyMbNvDzaRW3sjuunOQijDmbI6WfXB0wRgKOOdEKqzy8hoNXxhUHF-jQdz1Xpj6Zyh'
-
 class Bot(commands.Bot):
     """
     This is the code that will be executed when the bot is started.
@@ -33,29 +31,18 @@ class Bot(commands.Bot):
         """
         Tell the bot to start running. Reconnect is enabled by default.
         """
-        # print("Running bot...")
         super().run(token, reconnect=reconnect)
         
     async def close(self) -> None:
-        # print("Closing bot...")
         time.sleep(2)
         self.scheduler.shutdown()
         await super().close()
-        
-    # async def on_connect(self) -> None:
-    #     print(f"Bot connected! DWSP: {self.ws.latency * 1000:.0f}ms")
-        
-    # async def on_disconnect(self) -> None:
-    #     print("Bot disconnected!")
         
     async def on_ready(self) -> None:
         if self.ready:
             return
         
         self.scheduler.start()
-        # print(f"Scheduler started ({len(self.scheduler.get_jobs())} jobs)")
-        
-        # print(f"Bot is ready! Connected to: {self.user}")
         self.ready = True
         
     async def on_message(self, message: discord.Message) -> None:
@@ -71,20 +58,3 @@ class Bot(commands.Bot):
             return
         
         await self.invoke(ctx)
-
-# bot = Bot()
-        
-# class Help(commands.Cog):
-#     __slots__ = ()
-        
-#     @commands.command(name='help', 
-#                       help="View the help menu", 
-#                       usage="")
-#     async def command_help(self, ctx: commands.Context) -> None:
-#         await ctx.message.delete()
-#         await ctx.send(f"Hi")
-        
-# bot.add_cog(Help(bot))
-    
-# if __name__ == '__main__':
-#     bot.run()
